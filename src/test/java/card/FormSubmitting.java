4ef4840a6d64abb98df6c4c29724020223598aa2
astsidvan <astsidvan@gmail.com>
2022-10-12 20:09:48 +0300

    удаление driver

--- src/test/java/card/FormSubmitting.java
+++ src/test/java/card/FormSubmitting.java
@@ -1,11 +1,7 @@
 package card;
 import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -15,35 +11,18 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FormSubmitting {
 
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-
-        driver = new ChromeDriver(options);
-    }
-    @AfterEach
-    void teardown() {
-        driver.quit();
+        open("http://localhost:9999/");
     }
 
-    private String generateDate(int days) {
-        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-
 
     @Test
     void FormTest() {
-        String date = generateDate(3);
-        open("http://localhost:9999/");
+
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id=date] input").setValue(generateDate(3));
